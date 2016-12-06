a17b88a8103416fc83d0636a7e74840b7c429bfd
Xiaoqing Ma <Xiaoqing Ma>
2016-12-05 22:15:45 -0500

    add circuit simulation files and slides

---
+++ IRSIM/cmd files/script_decoder_ksa.cmd
@@ -0,0 +1,105 @@
+stepsize 30
+h vdd
+l gnd
+vector r0 r0[5] r0[4] r0[3] r0[2] r0[1] r0[0]
+vector r1 r1[5] r1[4] r1[3] r1[2] r1[1] r1[0]
+vector r2 r2[5] r2[4] r2[3] r2[2] r2[1] r2[0]
+vector r3 r3[5] r3[4] r3[3] r3[2] r3[1] r3[0]
+vector r4 r4[5] r4[4] r4[3] r4[2] r4[1] r4[0]
+vector r5 r5[5] r5[4] r5[3] r5[2] r5[1] r5[0]
+vector r6 r6[5] r6[4] r6[3] r6[2] r6[1] r6[0]
+vector r7 r7[5] r7[4] r7[3] r7[2] r7[1] r7[0]
+vector m m[3] m[2] m[1] m[0]
+analyzer r0 r1 r2 r3 r4 r5 r6 r7 m
+set r0 111000
+set r1 000001
+set r2 111111
+set r3 111001
+set r4 000100
+set r5 000101
+set r6 111010
+set r7 000111
+s
+set r0 000011
+set r1 000000
+set r2 000011
+set r3 001000
+set r4 000010
+set r5 000001
+set r6 000011
+set r7 000101
+s
+set r0 111110
+set r1 111100
+set r2 000011
+set r3 000000
+set r4 111110
+set r5 000110
+set r6 111011
+set r7 000000
+s
+set r0 111110
+set r1 111111
+set r2 111111
+set r3 000001
+set r4 111001
+set r5 111101
+set r6 111111
+set r7 111000
+s
+set r0 111011
+set r1 111100
+set r2 111000
+set r3 000101
+set r4 111100
+set r5 001001
+set r6 000101
+set r7 000011
+s
+set r0 000100
+set r1 111010
+set r2 110100
+set r3 111111
+set r4 001001
+set r5 000111
+set r6 000000
+set r7 111010
+s
+set r0 111101
+set r1 111001
+set r2 000010
+set r3 111111
+set r4 001000
+set r5 111010
+set r6 000111
+set r7 111111
+s
+set r0 000011
+set r1 000000
+set r2 111111
+set r3 111011
+set r4 000011
+set r5 111000
+set r6 111010
+set r7 111101
+s
+set r0 000010
+set r1 001001
+set r2 000000
+set r3 110110
+set r4 000100
+set r5 000011
+set r6 110100
+set r7 001000
+s
+set r0 000011
+set r1 000110
+set r2 000101
+set r3 111011
+set r4 001000
+set r5 111100
+set r6 111110
+set r7 000001
+s
+
+
---
+++ IRSIM/cmd files/script_gate_2input.cmd
@@ -0,0 +1,13 @@
+stepsize 30
+h vdd
+l gnd
+vector in a b
+analyzer in y
+set in 00
+s
+set in 10
+s
+set in 11
+s
+set in 01
+s
